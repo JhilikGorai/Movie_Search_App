30a55e8b7e829bd4db296d13aa469c03d4a55c47
JhilikGorai <jhilikgorai2008@gmail.com>
2026-08-19 17:08:10 +0530

    Update javascript

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ search.addEventListener("input" , () => {
             container.innerHTML = "";
             return;
         }
+
         try {
+            container.innerHTML = "<p>Loading...</p>"
+
         const result = await fetch(url);
         const data = await result.json();
 
@@ -26,14 +29,23 @@ search.addEventListener("input" , () => {
         data.Search.forEach((movie) => {
             const card = document.createElement("div");
             card.classList.add("movie-card");
+            card.dataset.id = movie.imdbID;
             
             card.innerHTML = `
-            <img src ="${movie.Poster}" alt="${movie.Title}">
+            <img src="${movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/180x260?text=No+Image'}" alt="${movie.Title}">
             <h3>${movie.Title}</h3>
             <p>${movie.Year}</p>
              `;
             container.appendChild(card);
+         });
+
+            container.addEventListener("click", (event) => {
+                const card = event.target.closest(".movie-card");
+                if (!card)
+                    return;
+                console.log("You clicked:", card.dataset.id);
             });
+            
         }  catch (error) {console.log("Something went wrong:", error.message);
 
         }
